8639fabdb285d1fcd7a19b2b242a71253d75d740
Bruno Ventura <brun0ventur4@gmail.com>
2015-04-27 14:23:24 -0300

    jogo pronto

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var model = {
+	turn: "",
+	whiteLeft: 0,
+	blackLeft: 0,
 
 	board: {
 		rows: 0,
@@ -9,12 +12,70 @@ var model = {
 
 		positions: [],
 
-		drawTable: function(rows, columns) {
-			board = document.querySelector("main").appendChild(document.createElement("div"));
-			board.className = "board";
+		addWhitePieces: function(piecesPerSide) {
+			model.whiteLeft = piecesPerSide;
+			var remaining = piecesPerSide;
+			var currentRow = 0;
+			var currentColumn = 1;
+			while(remaining > 0)
+			{
+				this.positions[currentRow][currentColumn] = new model.WhitePiece(currentRow, currentColumn);
+				if(currentColumn == this.columns-1)
+					currentColumn++;
+				else if(currentColumn == this.columns-2)
+					currentColumn += 3;
+				else
+					currentColumn += 2;
+				if(currentColumn >= this.columns)
+					currentRow++;
+				currentColumn = currentColumn % this.columns;
+				remaining--;
+			}
+		},
+
+		addBlackPieces: function(piecesPerSide) {
+			model.blackLeft = piecesPerSide;
+			var remaining = piecesPerSide;
+			var currentRow = this.rows-1;
+			var currentColumn = this.columns-2;
+			while(remaining > 0)
+			{
+				this.positions[currentRow][currentColumn] = new model.BlackPiece(currentRow, currentColumn);
+				currentColumn-=2;
+				if(currentColumn < 0)
+					currentRow--;
+				if(currentColumn == -1)
+					currentColumn = this.columns - 2;
+				else if(currentColumn == -2)
+					currentColumn = this.columns - 1;
+				currentColumn = currentColumn % this.columns;
+				remaining--;
+			}
+		},
+
+		initBoard: function(rows, columns, piecesPerSide){
+			model.turn = "white";
 
 			this.rows = rows;
 			this.columns = columns;
+			for(var i = 0; i < rows; i++) {
+				this.positions[i] = [];
+				for(var j = 0; j < columns; j++)
+					this.positions[i][j] = new model.Piece(i, j);
+			}
+			this.addWhitePieces(piecesPerSide);
+			this.addBlackPieces(piecesPerSide);
+		},
+
+		drawBoard: function() {
+			while (document.querySelector("main").firstChild)
+				document.querySelector("main").removeChild(document.querySelector("main").firstChild);
+
+			board = document.querySelector("main").appendChild(document.createElement("div"));
+			board.className = "board";
+
+			var rows = this.rows;
+			var columns = this.columns;
 
 			actualRow = 0;
 			actualColumn = 0;
@@ -24,77 +85,103 @@ var model = {
 				cell.setAttribute("row", actualRow);
 				cell.setAttribute("column", actualColumn);
 				cell.className = isWhite ? 'cell cell-white' : 'cell cell-black';
-				cell.addEventListener('click', this.selectCell);
-				board.appendChild(cell);	 
-
-				if (actualColumn == 0)
-					this.positions[actualRow] = [];
 				
-				this.positions[actualRow][actualColumn] = new model.Piece(actualRow, actualColumn);
-
+				board.appendChild(cell);
 				actualColumn++;
 				if (actualColumn >= columns) {
 					actualColumn = 0;
 					actualRow++;
 				}
 			}
+
+			for(var i = 0; i < rows; i++) {
+				for(var j = 0; j < columns; j++) {
+					var cell = document.querySelectorAll('div[row="'+i+'"][column="'+j+'"]')[0];
+					if(!this.positions[i][j].isEmpty()) {
+						var newPiece = document.createElement('img');
+						newPiece.setAttribute('src', this.positions[i][j].src)
+						cell.appendChild(newPiece);
+						if(this.positions[i][j].selected)
+							cell.className += ' selected';
+					}
+					if(this.positions[i][j].highlighted) {
+						cell.className += ' highlight';
+						var that = this;
+						var highlightedPiece = this.positions[i][j];
+						cell.addEventListener('click', function() {
+							that.selected.changePosition(parseInt(this.getAttribute('row')), parseInt(this.getAttribute('column')));	
+							that.unselectCell();
+							that.drawBoard();
+						});
+					} else {
+						var that = this;
+						cell.addEventListener('click', function(){
+							that.selectCell(this.getAttribute('row'), this.getAttribute('column'));
+							that.drawBoard();
+						});
+					}
+				}
+			}
+
+			//	this.draw();
 		},
 
-		putPieces: function (pieces) {
-			this.pieces = pieces;
+		putInitialPieces: function (pieces) {
+			var remaining = pieces;
+			var currentRow = 0;
+			var currentColumn = 1;
+			while(remaining > 0)
+			{
+				this.positions[currentRow][currentColumn] = new model.WhitePiece(currentRow, currentColumn);
+				remaining--;
+				currentColumn += 2;
+				if(currentColumn > this.columns)
+					currentRow++;
+				currentColumn = currentColumn % this.columns;
 
-			blackCells = [].slice.call(document.getElementsByClassName("cell-black"));
-			whiteCells = [].slice.call(document.getElementsByClassName("cell-black"));
-			whiteCells.reverse();
+			}
+			
+		},
 
-			totalBlack = 0;
-			totalWhite = 0;
-
-			for (var i=0; i<this.rows*this.columns/2; i++) {
-
-				blackRow = blackCells[i].getAttribute("row");
-				blackColumn = blackCells[i].getAttribute("column");
-				whiteRow = whiteCells[i].getAttribute("row");
-				whiteColumn = whiteCells[i].getAttribute("column");
-
-				if (totalBlack < pieces) {
-					this.positions[blackRow][blackColumn] = new model.WhitePiece(blackRow, blackColumn);
-					totalBlack++;
-				}
-
-				if (totalWhite < pieces) {
-					this.positions[whiteRow][whiteColumn] = new model.BlackPiece(whiteRow, whiteColumn);
-					totalWhite++;
+		draw: function () {
+			for (var i in this.positions) {
+				for (var j in this.positions[i]) {
+					this.positions[i][j].draw();
 				}
 			}
 		},
-		selectCell: function (event) {
-			console.log("aqui");
-			var cell = this;
-			var row = cell.getAttribute("row");
-			var column = cell.getAttribute("column");
-			var selected = model.board.selected;
 
-			model.board.clearHighlights();
-			var piece = model.board.positions[row][column];
-			if (selected instanceof model.Piece) {
-				selected.selected = false;
-				selected.findCell().className = selected.findCell().className.replace(/\b selected\b/,'');
+		selectCell: function (row, column) {
+			var cell = document.querySelectorAll('div[row="'+row+'"][column="'+column+'"]')[0];
+			if(this.selected != null) {
+				this.selected.selected = false;
+				this.clearHighlights();
 			}
-
-			if (!piece.isEmpty()) {
-				piece.selected = true;
-				model.board.selected = piece;
-				cell.className += " selected";
-				piece.highlightMoves();
+			this.selected = null;
+			
+			if(!this.positions[row][column].isEmpty() && model.turn == this.positions[row][column].color) {				
+				this.selected = this.positions[row][column];
+				this.selected.selected = true;
+				this.selected.highlightMoves(this.selected.row, this.selected.column);
 			}
+			this.drawBoard();
+		},
 
+		unselectCell: function() {
+			if(this.selected != null)
+				this.selected.selected = false;
+			this.selected = null;
+			this.clearHighlights();
 		},
 
 		clearHighlights: function () {
-			[].slice.call(document.getElementsByClassName("highlight")).forEach(function (element, index, array) {
-				element.className = element.className.replace(/\b highlight\b/,'');
-			});
+			for(var i = 0; i < this.rows; i++)
+			{
+				for(var j = 0; j < this.columns; j++)
+				{
+					this.positions[i][j].highlighted = false;
+				}
+			}
 		},
 
 		isOutOfBounds: function (row, column) {
@@ -108,11 +195,33 @@ var model = {
 		this.column = parseInt(column);
 		this.src = "";
 		this.selected = false;
+		this.highlighted = false;
 
-		this.changePosition = function () {
-			var row = this.getAttribute("row");
-			var column = this.getAttribute("column");
-			console.log(row, column, this);
+		this.changePosition = function (newRow, newColumn) {
+			model.board.positions[this.row][this.column] = new model.Piece(this.row, this.column);
+			if (Math.abs(this.row - newRow) > 1 || Math.abs(this.row - newRow) > 1) {
+				var eatenRowPos = (this.row + newRow)/2;
+				var eatenColumnPos = (this.column + newColumn)/2;
+				model.board.positions[eatenRowPos][eatenColumnPos] = new model.Piece(eatenRowPos, eatenColumnPos);
+				if (this.color == "white") {
+					model.blackLeft--;
+				} else {
+					model.whiteLeft--;
+				}
+			} 
+
+			this.row = newRow;
+			this.column = newColumn; 
+			model.board.positions[this.row][this.column] = this;
+
+			model.turn = model.turn == "white" ? "black" : "white";
+
+			if (this.turnDama())
+				if (model.turn == "black") {
+					model.board.positions[this.row][this.column] = new model.WhiteDama(this.row, this.column);
+				} else {
+					model.board.positions[this.row][this.column] = new model.BlackDama(this.row, this.column);
+				}
 		}
 
 		this.draw = function () {
@@ -127,17 +236,13 @@ var model = {
 			if(!model.board.isOutOfBounds(row, column)) {
 				var cellTo = model.board.positions[row][column];
 				if (cellTo.isEmpty()) {
-					cellTo.findCell().addEventListener('click', this.changePosition);
-					cellTo.findCell().className += " highlight";
+					cellTo.highlighted = true;
+					return false;
+				} if (this.color == model.board.positions[row][column].color) {
+					return false;
 				}
+				return true;
 			}
-		}
-
-		this.highlightMoves = function () {
-			this.highlightMove(this.row+1, this.column+1);
-			this.highlightMove(this.row-1, this.column-1);
-			this.highlightMove(this.row-1, this.column+1);
-			this.highlightMove(this.row+1, this.column-1);
 		}
 
 		this.isEmpty = function () {
@@ -145,42 +250,139 @@ var model = {
 		}
 
 		this.findCell = function () {
-			return document.querySelectorAll('div[row="'+row+'"][column="'+column+'"]')[0];
+			return document.querySelectorAll('div[row="'+this.row+'"][column="'+this.column+'"]')[0];
 		}
-
-		this.draw();
 
 	},
 
 	BlackPiece: function (row, column) {
 		this.__proto__ = new model.Piece(row, column);
 		this.src = 'img/black.png';
+		this.color = "black";
+
+		this.highlightMoves = function (row, column) {
+
+			if (this.highlightMove(row-1, column-1))
+				this.highlightMove(row-2, column-2);
+
+			if (this.highlightMove(row-1, column+1))
+				this.highlightMove(row-2, column+2);
+
+		}
+
+		this.turnDama = function () {
+			console.log(this.row, model.board.rows);
+			return this.row == 0;
+		}
 
 		this.isEmpty = function () {
 			return false;
 		}
-
-		this.draw();
 
 	},
 
 	WhitePiece: function (row, column) {
 		this.__proto__ = new model.Piece(row, column);
 		this.src = 'img/white.png';
+		this.color = "white";
+
+		this.highlightMoves = function (row, column) {
+
+			if (this.highlightMove(row+1, column+1))
+				this.highlightMove(row+2, column+2);
+
+			if (this.highlightMove(row+1, column-1))
+				this.highlightMove(row+2, column-2);
+		}
+
+		this.turnDama = function () {
+			console.log(this.row, model.board.rows);
+			return this.row == model.board.rows-1;
+		}
 
 		this.isEmpty = function () {
 			return false;
 		}
 
-		this.draw();
+	},
+
+	BlackDama: function (row, column) {
+		this.__proto__ = new model.BlackPiece(row, column);
+		this.src = 'img/blackdama.png';
+
+		this.highlightMoves = function (row, column) {
+
+			if (this.highlightMove(row+1, column+1))
+				this.highlightMove(row+2, column+2);
+
+			if (this.highlightMove(row-1, column-1))
+				this.highlightMove(row-2, column-2);
+
+			if (this.highlightMove(row-1, column+1))
+				this.highlightMove(row-2, column+2);
+
+			if (this.highlightMove(row+1, column-1))
+				this.highlightMove(row+2, column-2);
+		}
+
+		this.isEmpty = function () {
+			return false;
+		}
+
+	},
+
+	WhiteDama: function (row, column) {
+		this.__proto__ = new model.WhitePiece(row, column);
+		this.src = 'img/whitedama.png';
+
+		this.highlightMoves = function (row, column) {
+
+			if (this.highlightMove(row+1, column+1))
+				this.highlightMove(row+2, column+2);
+
+			if (this.highlightMove(row-1, column-1))
+				this.highlightMove(row-2, column-2);
+
+			if (this.highlightMove(row-1, column+1))
+				this.highlightMove(row-2, column+2);
+
+			if (this.highlightMove(row+1, column-1))
+				this.highlightMove(row+2, column-2);
+		}
+
+		this.isEmpty = function () {
+			return false;
+		}
 
 	}
 
 };
 
-var init = function() {
-	model.board.drawTable(8,8);
-	model.board.putPieces(5);
+var init = function() { 
+	model.board.initBoard(8, 8, 12); 
+	model.board.drawBoard();
 }
+
+Object.observe(model, function(changes){
+    changes.forEach(function(change) {
+    	if (change.name == "turn")
+        	document.querySelectorAll('#info h1 span')[0].innerHTML = model.turn == "white" ? "Branco": "Preto";
+ 
+        if (change.name == "whiteLeft" || change.name == "blackLeft") {
+        	document.querySelectorAll('#white-counter span')[0].innerHTML = model.whiteLeft;
+        	document.querySelectorAll('#black-counter span')[0].innerHTML = model.blackLeft;
+        	
+        	if (model.whiteLeft == 0) {
+        		alert("Jogador Preto Ganhou!");
+        		init();
+        	}
+        	if (model.blackLeft == 0) {
+        		alert("Jogador Branco Ganhou!");
+        		init();
+        	}
+        }
+    });
+
+});
 
 init();
